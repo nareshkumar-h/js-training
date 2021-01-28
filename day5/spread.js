c21a76ee6c24df35f888f8ae9e0eccfa2b50688e
Naresh Kumar H <nareshkumarh@live.com>
2021-01-28 16:55:56 +0530

    Added Spread

---
+++ day5/spread.js
@@ -0,0 +1,13 @@
+//Spread syntax can be used when all elements from an object  or array need to be included in a list.
+
+var obj1= { trainNo: 1, trainName:"Train 1"}
+
+var obj2 = {trainNo: 1, timings: "10:30"}
+
+var result = { ...obj1}
+console.log(result);
+//{trainNo: 1, trainName: "Train 1"}
+
+var result1= { ...obj1,...obj2}
+console.log(result1);
+//{trainNo: 1, trainName: "Train 1", timings: "10:30"}
