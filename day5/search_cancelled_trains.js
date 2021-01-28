078ea0c706f90e0d185aaa0c19d34742ac778d43
Naresh Kumar H <nareshkumarh@live.com>
2021-01-28 10:55:46 +0530

    Updated

--- day5/search_cancelled_trains.js
+++ day5/search_cancelled_trains.js
@@ -1,12 +1,24 @@
 var trains = [
-    { trainNo:123, from:"Chennai Central",to: "Bangalore", type:"DAILY"},
-    { trainNo:124, from:"Chennai Central",to: "Bangalore", type:"MON,THU"},
-    { trainNo:125, from:"Chennai Central",to: "Bangalore", type:"WED"}
+    { trainNo:123, from:"Chennai Central",to: "Bangalore" },
+    { trainNo:124, from:"Chennai Central",to: "Bangalore" },
+    { trainNo:125, from:"Chennai Central",to: "Bangalore"}
 ];
+
+var trainSchedules = [
+    {trainNo: 123, type:"DAILY"},
+    {trainNo:124, type:"MON,THU,WED"},
+    {trainNo:125, type:"MON"}
+]
 
 var cancelledTrains = [ 
     {trainNo: 123, cancelledOn:"2021-01-28"}
 ];
+
+function isCancelled(trainNo, journeyDate){
+    let cancelled = false;
+    //todo: logic
+    return cancelled;
+}
 
 function getDay(date){
     return date.toString().substring(0,3).toUpperCase();
@@ -19,16 +31,13 @@ function isTrainRunningOnThatDay(type, journeyDate){
     return type=="DAILY" || days.includes(journeyDay);
 }
 
-function isCancelled(trainNo, journeyDate){
-    let cancelled = false;
-    //todo: logic
-    return cancelled;
-}
+
 
 function searchTrains(source, destination, journeyDateStr){
 
     let journeyDate = new Date(journeyDateStr);            
     let filteredTrains = trains.filter(t => !isCancelled(t.trainNo,journeyDateStr) && isTrainRunningOnThatDay(t.type, journeyDate,t.status));       
+
     return filteredTrains;
 }
 
