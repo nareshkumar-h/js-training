1168667ed52a9200ee905064bb5722e2866f6438
Naresh Kumar H <nareshkumarh@live.com>
2021-01-28 17:00:16 +0530

    Updated

--- day5/spread.js
+++ day5/spread.js
@@ -11,3 +11,11 @@ console.log(result);
 var result1= { ...obj1,...obj2}
 console.log(result1);
 //{trainNo: 1, trainName: "Train 1", timings: "10:30"}
+
+var trains = [1,2,3]
+let newTrains = [...trains]
+console.log(newTrains);
+
+var trainsObjArray = [{trainNo:1}, {trainNo:2}]
+let newTrainsArray = [... trainsObjArray];
+console.log(newTrainsArray);
