ec7c104424823b73b94d56380e98a513b62934ce
Naresh Kumar H <nareshkumarh@live.com>
2021-01-28 14:55:07 +0530

    Updated

--- day5/sort_trains.js
+++ day5/sort_trains.js
@@ -1,7 +1,9 @@
 var trains = [
     { trainNo:123, from:"Chennai Central",to: "Bangalore" },
     { trainNo:124, from:"Chennai Central",to: "Bangalore" },
-    { trainNo:125, from:"Chennai Central",to: "Bangalore"}
+    { trainNo:125, from:"Chennai Central",to: "Bangalore"},
+    { trainNo:126, from:"Chennai Central",to: "Erode"},
+    { trainNo:127, from:"Chennai Central",to: "Hyderabad"},
 ];
 
 var trainSchedules = [
@@ -11,6 +13,7 @@ var trainSchedules = [
     {trainNo:126, type:"MON", timings: "15:00"},
     {trainNo:127, type:"MON", timings: "10:00"},
 ];
+//{ trainNo:123, from:"Chennai Central", to:"Bangalore", type:"DAILY", timings:"23:30"}
 
 function sortByTimingsASC(t1,t2){
     
@@ -40,9 +43,18 @@ function sortByTimingsDESC(t1,t2){
     return value;
 }
 
-let results= searchTrains("Chennai Central","Bangalore","2020-01-27");
+function searchTrains(source, destination){
+    let filteredTrains  = [];
+    return filteredTrains;
+}
+
+let results= searchTrains("Chennai Central","Bangalore");
 console.log(results);
 
+let sortByTimings = "DESC";
+//IF DESC , sort by descending, ASC sort by ascending
 results.sort(sortByTimingsASC);
 
 console.log("Sorted Trains:", results);
+
+
