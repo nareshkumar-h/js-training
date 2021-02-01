e3b153b2230f8b01cfb755589691bcfcd42887c7
Naresh Kumar H <nareshkumarh@live.com>
2021-02-01 12:35:11 +0530

    Updated

--- day6/html_generator.js
+++ day6/html_generator.js
@@ -4,10 +4,18 @@ let template =
 <!doctype HTML>
 <html>
 <head>
-<title>${title}</title>
+<title id="title">${title}</title>
 </head>
 <body>
 <h3>${title}</h3>
+<script>
+
+function getTitle(){
+
+    var title = document.querySelector("#title");
+    console.log(title);
+}
+</script>
 </body>
 </html>`;
 console.log(template);
