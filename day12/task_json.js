e38d4aa60b5f496c63b293aceed6c1f875b6dba8
Naresh Kumar H <nareshkumarh@live.com>
2021-02-08 10:26:34 +0530

    Updated

--- day12/task_json.js
+++ day12/task_json.js
@@ -4,21 +4,43 @@ var data = [{
         { "fieldName":"id", "type":"number"},
         { "fieldName":"name", "type":"text", "size":100},
         { "fieldName":"email", "type":"text", "size":100},
-        { "fieldName":"password", "type":"text", "size":100},
+        { "fieldName":"password", "type":"text", "size":50},
     ]
 }];
 
 const fs = require('fs');
 
 let content = "";
+
 for(let tableObj of data ){
     console.log(tableObj);
+    let i = 1;
     content+= `create table ${tableObj.tableName} ( \n`;    
     for(let fieldObj of tableObj.fields){
 
-        content+= `${fieldObj.fieldName} ${fieldObj.type}(${fieldObj.size}),`;
+        content+= `${fieldObj.fieldName} `;
+        
+        let dataType = null;
+        switch(fieldObj.type){
+            case "number": 
+            dataType = 'int'; break;
+            case "text": 
+            dataType = 'varchar'; break;
+            default:
+                dataType = fieldObj.type;
+        }
+            content+=`${dataType}`;
+        
+        if (fieldObj.size){
+            content+=`(${fieldObj.size})`;
+        }
+        if( i != tableObj.fields.length){
+            content +=`,`;
+        }
+        
+        i++;
     }
-    content+= `);`;    
+    content+= `\n);`;    
 }
 //implement logic
 console.log(content);
